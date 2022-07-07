06b0f8dfeaef01ef63ca54ee26899edd49dfded0
Juliana Batista <julianabatista0807@gmail.com>
2022-07-07 11:23:54 -0300

    Atualizei o código para finalizar quando a pessoa acerta e restringir os numeros de chute para 1 até 100

--- adivinhacao-alura.py
+++ adivinhacao-alura.py
@@ -18,6 +18,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (chute == numero_secreto):
         print("Você acertou!")
+        break
     else:
         if(chute > numero_secreto):
             print("O seu chute foi maior do que o número secreto!")
